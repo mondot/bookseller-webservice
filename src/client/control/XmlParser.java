6a182b8a9fc8f371505f38f84aafc71ab739ce03
MONDOT Quentin <quent.mondot@hotmail.fr>
2016-10-25 23:11:44 +0300

    Created the XmlParser class and the getValeuFromTagNameAndStringXml method

---
+++ src/client/control/XmlParser.java
@@ -0,0 +1,37 @@
+package client.control;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class XmlParser {
+
+	protected static String getValeuFromTagNameAndStringXml(String tagName, String xml) throws ParserConfigurationException, SAXException, IOException {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(new InputSource(new StringReader(xml)));
+		Element element = document.getDocumentElement();
+
+        NodeList list = element.getElementsByTagName(tagName);
+
+        if (list != null && list.getLength() > 0) {
+            NodeList subList = list.item(0).getChildNodes();
+
+            if (subList != null && subList.getLength() > 0) {
+                return subList.item(0).getNodeValue();
+            }
+        }
+
+        return null;
+    }
+}
